0100b153306e2e28523c92328abaf92220e5dd3b
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-03-04 21:24:24 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //interface => defines a contract that a class must follow. It specifies what a class can do, not how it does it.
+            // To follow two important Principle
+            //(1):Program to an interface, not an implementation
+            //(2):Dependency Inversion Principle
+
+            // Enable polymorphism without inheritance
+            // Remove tight coupling between classes
+            // Enable multiple inheritance. 
+
+            #endregion
         }
     }
 }
